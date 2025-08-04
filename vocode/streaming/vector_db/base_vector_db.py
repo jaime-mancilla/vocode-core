7dd49f4825f970895a384d6aafd663e686249e18
Jaime Mancilla <jaime.mancilla@horizonroadrescue.com>
2025-08-04 03:59:27 -0500

    Update base_vector_db.py to replace Async Features

--- vocode/streaming/vector_db/base_vector_db.py
+++ vocode/streaming/vector_db/base_vector_db.py
@@ -1,10 +1,8 @@
 import os
-from typing import TYPE_CHECKING, Iterable, List, Optional, Tuple, Union
+from typing import TYPE_CHECKING, Iterable, List, Optional, Tuple
 
 import aiohttp
-from openai import AsyncAzureOpenAI, AsyncOpenAI
-
-from vocode.streaming.models.agent import AZURE_OPENAI_DEFAULT_API_VERSION
+import openai
 
 if TYPE_CHECKING:
     from langchain.docstore.document import Document
@@ -13,14 +11,11 @@ DEFAULT_OPENAI_EMBEDDING_MODEL = "text-embedding-ada-002"
 
 
 class VectorDB:
-    openai_client: Union[AsyncOpenAI, AsyncAzureOpenAI]
-
     def __init__(
         self,
         aiohttp_session: Optional[aiohttp.ClientSession] = None,
     ):
         if aiohttp_session:
-            # the caller is responsible for closing the session
             self.aiohttp_session = aiohttp_session
             self.should_close_session_on_tear_down = False
         else:
@@ -28,29 +23,21 @@ class VectorDB:
             self.should_close_session_on_tear_down = True
 
         self.engine = os.getenv("AZURE_OPENAI_TEXT_EMBEDDING_ENGINE")
+        self.api_key = os.getenv("AZURE_OPENAI_API_KEY_EAST_US") if self.engine else os.getenv("OPENAI_API_KEY")
+        openai.api_key = self.api_key
         if self.engine:
-            azure_base = os.getenv("AZURE_OPENAI_API_BASE_EAST_US")
-            azure_base = azure_base if azure_base is not None else ""
-            self.openai_client = AsyncAzureOpenAI(
-                azure_endpoint=azure_base,
-                api_key=os.getenv("AZURE_OPENAI_API_KEY_EAST_US"),
-                api_version=AZURE_OPENAI_DEFAULT_API_VERSION,
-            )
-        else:
-            self.openai_client = AsyncOpenAI(
-                api_key=os.getenv("OPENAI_API_KEY"),
-            )
+            openai.api_base = os.getenv("AZURE_OPENAI_API_BASE_EAST_US")
+            openai.api_type = "azure"
+            openai.api_version = os.getenv("AZURE_OPENAI_API_VERSION", "2023-05-15")
 
-    async def create_openai_embedding(
+    def create_openai_embedding(
         self, text, model=DEFAULT_OPENAI_EMBEDDING_MODEL
     ) -> List[float]:
-        params = {
-            "input": text,
-        }
-
-        params["model"] = self.engine if self.engine else model
-
-        return (await self.openai_client.embeddings.create(**params)).data[0].embedding
+        response = openai.Embedding.create(
+            input=text,
+            model=self.engine if self.engine else model,
+        )
+        return response["data"][0]["embedding"]
 
     async def add_texts(
         self,
